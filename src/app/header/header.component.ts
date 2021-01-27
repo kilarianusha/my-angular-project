23fe24fb03bda25f555ad4ebecacc54191502fd3
Anusha Kilari <kilarianusha@Anushas-Air.fios-router.home>
2021-01-26 19:09:47 -0500

    first commit of angular

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+  isCollapsed = false;
+  @Output() featureSelected = new EventEmitter<string>();
+  constructor() { }
+  
+  onSelect(feature :string){
+    this.featureSelected.emit(feature);
+
+  }
+}
