89afaffd92206bde350dd421cb6eba13a613e1f0
Anusha Kilari <kilarianusha@Anushas-Air.fios-router.home>
2021-01-29 15:28:28 -0500

    adding shopping list and recipes details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {  Recipe } from '../recipe.model';
+import { Recipe } from './../recipe.model';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,9 +7,10 @@ import {  Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter< Recipe>();
   recipes:Recipe[] =[
   new Recipe('A Test Recipe','This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-  new Recipe('A Test Recipe','This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+  new Recipe('A another Recipe','This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
   
   ];
 
@@ -17,5 +18,8 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  onRecipeSelected(recipe:Recipe){
+this.recipeWasSelected.emit(recipe);
 
+  }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -14,5 +14,8 @@ ingredients : Ingredient[] = [
 
   ngOnInit(): void {
   }
+  onIngredientAdded(ingredient: Ingredient){
+    this.ingredients.push(ingredient);
 
+  }
 }
